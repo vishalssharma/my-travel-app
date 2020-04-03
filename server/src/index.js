74f7468c3d2c83c011b549cc568bcfb5cb8de913
Vishal <vishalsharma.vs@hotmail.com>
2020-04-03 23:56:29 +0530

    added new end points

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,4 @@
-const middlewares = require('./middlewares')
+const middlewares = require('./middlewares');
 const express = require('express');
 // clean logs
 const morgan = require('morgan');
@@ -6,20 +6,30 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 // cross origin resource sharing
 const cors = require('cors');
-
+const mongoose = require('mongoose');
+const logs = require('./api/logs');
+require('dotenv').config();
 const app = express();
+const uri = process.env.DATABASE_URL;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 app.use(morgan('common'));
 app.use(helmet());
 // set origin :: only req from this can reach our backend
 app.use(cors({
-    origin : 'http://locathost:3000'
+    origin: process.env.CORS_ORIGIN,
 }));
 
+app.use(express.json());
 app.get('/', (req,res) => {
     res.json({
         message: "Hello World",
     })
 })
+app.use('/api/logs',logs);
 // creating a Not Found error , set status and forward to actual error handler
 app.use(middlewares.notFound);
 
